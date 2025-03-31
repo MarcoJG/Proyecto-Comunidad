3f9e8a637ce84c6027a46a9e1627086549e7c698
Marco <marcojgb@gmail.com>
2025-03-31 22:13:08 +0200

    Cambios en todos los archivos

--- MySQL/BBDD_comunidad.sql
+++ MySQL/BBDD_comunidad.sql
@@ -1,21 +1,20 @@
 CREATE DATABASE IF NOT EXISTS proyecto_comunidad;
 USE proyecto_comunidad;
 
-CREATE TABLE usuario (
-id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-nombre_completo VARCHAR(100) NOT NULL,
-email VARCHAR(45) NOT NULL UNIQUE,
-username VARCHAR(50) NOT NULL UNIQUE,
-password VARCHAR(255) NOT NULL,
-id_roles INT NOT NULL
-);
 CREATE TABLE IF NOT EXISTS roles (
 id_roles INT AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR(45) NOT NULL
+nombre VARCHAR(100) NOT NULL
 );
-ALTER TABLE usuario
-ADD CONSTRAINT fk_usuario_roles
-FOREIGN KEY(id_roles) REFERENCES roles(id_roles);
+CREATE TABLE IF NOT EXISTS usuarios (
+id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+correo VARCHAR(100) NOT NULL UNIQUE,
+usuario VARCHAR(100) NOT NULL,
+contrasenya VARCHAR(255) NOT NULL,
+id_roles INT NOT NULL,
+CONSTRAINT fk_usuarios_roles FOREIGN KEY(id_roles) REFERENCES roles(id_roles)
+);
+
 CREATE TABLE IF NOT EXISTS noticias (
 id_noticias INT AUTO_INCREMENT PRIMARY KEY,
 nombre_noticia VARCHAR(100) NOT NULL,
@@ -24,8 +23,7 @@ contenido TEXT NOT NULL,
 fecha DATE NOT NULL,
 es_destacada TINYINT(1) DEFAULT 0,
 id_usuario INT NOT NULL,
-CONSTRAINT fk_noticias_usuario FOREIGN KEY (id_usuario) REFERENCES
-usuario(id_usuario)
+CONSTRAINT fk_noticias_usuario FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario)
 );
 CREATE TABLE IF NOT EXISTS eventos (
 id_evento INT AUTO_INCREMENT PRIMARY KEY,
@@ -33,8 +31,8 @@ titulo VARCHAR(100) NOT NULL,
 descripción VARCHAR(255) NOT NULL,
 fecha DATE NOT NULL,
 id_usuario INT NOT NULL,
-CONSTRAINT fk_eventos_usuario FOREIGN KEY (id_usuario) REFERENCES
-usuario(id_usuario)
+CONSTRAINT fk_eventos_usuarios FOREIGN KEY (id_usuario) REFERENCES
+usuarios(id_usuario)
 );
 CREATE TABLE IF NOT EXISTS votacion (
 id_votacion INT AUTO_INCREMENT PRIMARY KEY,
@@ -42,17 +40,16 @@ descripción VARCHAR(255) NOT NULL,
 fecha_inicio DATETIME,
 fecha_fin DATETIME,
 id_usuario INT NOT NULL,
-CONSTRAINT fk_votacion_usuario FOREIGN KEY (id_usuario) REFERENCES
-usuario(id_usuario)
+CONSTRAINT fk_votacion_usuarios FOREIGN KEY (id_usuario) REFERENCES
+usuarios(id_usuario)
 );
 CREATE TABLE IF NOT EXISTS voto (
 id_voto INT AUTO_INCREMENT PRIMARY KEY,
 id_usuario INT NOT NULL,
-
 id_votacion INT NOT NULL,
 opciones ENUM('Sí', 'No') NOT NULL DEFAULT 'Sí',
-CONSTRAINT fk_voto_usuario FOREIGN KEY (id_usuario) REFERENCES
-usuario(id_usuario),
+CONSTRAINT fk_voto_usuarios FOREIGN KEY (id_usuario) REFERENCES
+usuarios(id_usuario),
 CONSTRAINT fk_voto_votacion FOREIGN KEY (id_votacion) REFERENCES
 votacion(id_votacion)
 );
@@ -62,17 +59,8 @@ motivo VARCHAR(255) NOT NULL,
 fecha_inicio DATETIME,
 fecha_fin DATETIME,
 id_usuario INT NOT NULL,
-CONSTRAINT fk_baneo_usuario FOREIGN KEY(id_usuario) REFERENCES
-usuario(id_usuario)
-);
-CREATE TABLE IF NOT EXISTS respuesta (
-id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
-contenido VARCHAR(255) NOT NULL,
-fecha DATE NOT NULL,
-id_hilo INT NOT NULL,
-id_usuario INT NOT NULL,
-CONSTRAINT fk_respuesta_usuario FOREIGN KEY(id_usuario) REFERENCES
-usuario(id_usuario)
+CONSTRAINT fk_baneo_usuarios FOREIGN KEY(id_usuario) REFERENCES
+usuarios(id_usuario)
 );
 CREATE TABLE IF NOT EXISTS hilo (
 id_hilo INT AUTO_INCREMENT PRIMARY KEY,
@@ -80,12 +68,18 @@ titulo VARCHAR(100) NOT NULL,
 fecha DATE NOT NULL,
 id_foro INT NOT NULL,
 id_usuario INT NOT NULL,
-CONSTRAINT fk_hilo_usuario FOREIGN KEY(id_usuario) REFERENCES
-usuario(id_usuario)
+CONSTRAINT fk_hilo_usuarios FOREIGN KEY(id_usuario) REFERENCES
+usuarios(id_usuario)
 );
-ALTER TABLE IF NOT EXISTS respuesta
-ADD CONSTRAINT fk_respuesta_hilo
-FOREIGN KEY (id_hilo) REFERENCES hilo(id_hilo);
+CREATE TABLE IF NOT EXISTS respuesta (
+id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
+contenido VARCHAR(255) NOT NULL,
+fecha DATE NOT NULL,
+id_hilo INT NOT NULL,
+id_usuario INT NOT NULL,
+CONSTRAINT fk_respuesta_usuarios FOREIGN KEY(id_usuario) REFERENCES
+usuarios(id_usuario)
+);
 CREATE TABLE foro (
 id_foro INT AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(100)
@@ -98,8 +92,8 @@ id_reserva INT AUTO_INCREMENT PRIMARY KEY,
 fecha_reserva DATETIME NOT NULL,
 zona VARCHAR(100),
 id_usuario INT NOT NULL,
-CONSTRAINT fk_reserva_zona_comun_usuario FOREIGN KEY(id_usuario) REFERENCES
-usuario(id_usuario)
+CONSTRAINT fk_reserva_zona_comun_usuarios FOREIGN KEY(id_usuario) REFERENCES
+usuarios(id_usuario)
 );
 CREATE TABLE IF NOT EXISTS incidencia (
 id_incidencia INT AUTO_INCREMENT PRIMARY KEY,
@@ -107,10 +101,6 @@ descripcion VARCHAR(255) NOT NULL,
 id_usuario INT NOT NULL,
 fecha_reporte DATETIME NOT NULL,
 estado ENUM('Pendiente', 'En proceso', 'Resuelta') NOT NULL DEFAULT 'PENDIENTE',
-CONSTRAINT fk_incidencia_usuario FOREIGN KEY(id_usuario) REFERENCES
-usuario(id_usuario)
+CONSTRAINT fk_incidencia_usuarios FOREIGN KEY(id_usuario) REFERENCES
+usuarios(id_usuario)
 );
-
--- Usuario de prueba: usuario = admin, contraseña = admin123
-INSERT INTO usuario (username, password) 
-VALUES ('admin', SHA2('admin123', 256));
---
+++ PHP/conexion_db_pm.php
@@ -0,0 +1,34 @@
+<?php 
+$host = 'localhost';
+$db = 'proyecto_comunidad';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host; dbname=$db;charset=$charset";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    die("Error en la conexión: " . $e->getMessage());
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if((!empty($_POST['username'])) && (!empty($_POST['password']))){
+        $username = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password']);
+
+        $stmt = $pdo->prepare("SELECT username FROM usuario WHERE username = :username and password = SHA2(:password, 256)");
+        $stmt->execute(['username' => $username, 'password' => $password]);
+        $user = $stmt->fetch();
+
+        if ($user) {
+            echo "Bienvenido, " . htmlspecialchars($user['username']);
+        } else {
+            echo "Usuario o contraseña incorrectos.";
+        }
+    }
+}
