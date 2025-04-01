d4d68cb27ea0e74a29ca0e3e302392a567525f46
Raquel Espinosa Garrido <reg0007@alu.medac.es>
2025-04-01 19:00:04 +0200

    style(registro): agregar hoja de estilos al formulario de registro

--- src/registro/formulario.php
+++ src/registro/formulario.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Formulario de Registro de Usuario</title>
+    <link rel="stylesheet" href="registro.css">
 </head>
+
 <body>
-    <h2>Regístrate</h2>
-    <form action="registro.php" method="POST">
-        <label>Nombre de Usuario:</label><br>
-        <input type="text" name="username" placeholder="Pepito..." required><br>
-        <label>Correo electrónico:</label><br>
-        <input type="email" name="email" required><br>
-        <label>Contraseña:</label><br>
-        <input type="password" name="password" required><br><br>
-        <input type="submit" value="Confirmar">
-    </form>
+   
+    <div class="registro-container">
+        <h2>Regístrate</h2>
+        <form action="registro.php" method="POST">
+            <label>Nombre de Usuario:</label><br>
+            <input type="text" name="username" placeholder="Ejemplo: Pepito" required><br><br>
+            <label>Correo electrónico:</label><br>
+            <input type="email" name="email" placeholder="Ejemplo: pepito@gmail.com" required><br><br>
+            <label>Contraseña:</label><br>
+            <input type="password" name="password" required><br><br>
+            <input type="submit" value="Confirmar">
+        </form>
 </body>
+
 </html>
