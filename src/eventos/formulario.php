1426f0ecd05c3f370f852d5362115100dc9f8b59
Raquel Espinosa Garrido <reg0007@alu.medac.es>
2025-04-06 12:08:28 +0200

    feat(eventos): agregar estructura HTML con imágenes asociadas

---
+++ src/eventos/formulario.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventos</title>
+    <link rel="stylesheet" href="Eventos.css">
+</head>
+
+<body class="fondo-cuerpo">
+
+    <!-- Aquí irá la cabecera -->
+
+    <div class="contenedor-principal">
+
+        <!-- Próximos Eventos -->
+        <div class="contenedor proximos-eventos">
+            <h2 class="titulo-eventos">Próximos eventos</h2>
+            <p class="subtitulo">Consulta todos los eventos de nuestra comunidad aquí</p>
+
+            <!--Evento 1 -->
+            <div class="evento">
+
+                <div class="evento-imagen">
+                    <img src="../../etc/assets/bloque.jpg" alt="ImagenEvento">
+                </div>
+
+                <!-- Texto y botón -->
+                <div class="evento-texto">
+                    <h2 class="titulo-evento">Próxima Junta de Propietarios</h2>
+                    <p class="detalle-evento">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+                        consequuntur ipsa aspernatur fugit suscipit aliquam beatae, voluptatum praesentium maxime
+                        doloremque! Sint quia dolores soluta molestias sunt. Quos iste quia quo!.</p>
+                    <button class="boton-evento">Accede</button>
+                </div>
+            </div>
+
+            <!-- Evento 2 -->
+            <div class="evento">
+
+                <div class="evento-imagen">
+                    <img src="../../etc/assets/reparar.jpg" alt="ImagenEvento">
+                </div>
+
+                <!-- Texto y botón -->
+                <div class="evento-texto">
+                    <h2 class="titulo-evento">Cambio de contadores</h2>
+                    <p class="detalle-evento">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod,
+                        exercitationem vel! Perferendis minus aliquam rerum dolore quam sequi necessitatibus explicabo.
+                    </p>
+                    <button class="boton-evento">Accede</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="contenedor-principal">
+
+        <!--  Eventos pasados -->
+        <div class="contenedor proximos-eventos">
+            <h2 class="titulo-eventos">Eventos pasados</h2>
+
+            <!-- Evento 1 -->
+            <div class="evento">
+                <div class="evento-imagen">
+                    <img src="../../etc/assets/bloque.jpg" alt="ImagenEvento">
+                </div>
+
+                <!-- Texto y botón -->
+                <div class="evento-texto">
+                    <h2 class="titulo-evento">Junta de Propietarios 10/12/2024</h2>
+                    <p class="detalle-evento">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+                        consequuntur ipsa aspernatur fugit suscipit aliquam beatae, voluptatum praesentium maxime
+                        doloremque! Sint quia dolores soluta molestias sunt. Quos iste quia quo!.</p>
+                    <button class="boton-evento">Accede</button>
+                </div>
+            </div>
+
+            <!-- Evento 2  -->
+            <div class="evento">
+                <div class="evento-imagen">
+                    <img src="../../etc/assets/ayuda.jpg" alt="ImagenEvento">
+                </div>
+
+                <!-- Texto y botón -->
+                <div class="evento-texto">
+                    <h2 class="titulo-evento">La Comunidad con los afectados por la DANA</h2>
+                    <p class="detalle-evento">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod,
+                        exercitationem vel! Perferendis minus aliquam rerum dolore quam sequi necessitatibus explicabo.
+                    </p>
+                    <button class="boton-evento">Accede</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Aquí irá el footer -->
+     
+</body>
+
+</html>
