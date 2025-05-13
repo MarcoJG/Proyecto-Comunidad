72c348ebc314b040e4b5e32e76eae626b6d15f57
rubenalcarazgadea <rag0020@alu.medac.es>
2025-05-13 11:09:49 +0200

    Hago la parte de borrar noticia

--- backend/src/noticias/detalle.php
+++ backend/src/noticias/detalle.php
@@ -30,6 +30,43 @@ if (isset($_GET['id']) && ctype_digit($_GET['id'])) {
             </div>
             <div class='descripcion-noticia'>
                 <p>{$contenido}</p>
+            <style>
+                            .btn-borrar img {
+                                width: 20px;
+                                height: 20px;
+                                margin-right: 8px;
+                            }
+                            .btn-borrar .icon-abierto {
+                                display: none;
+                            }
+                            .btn-borrar:hover .icon-cerrado {
+                                display: none;
+                            }
+                            .btn-borrar:hover .icon-abierto {
+                                display: inline;
+                                scale : 1.2;
+                            }
+                            .contenedor-boton {
+                                text-align: right;
+                                margin-top: 10px;
+                            }
+                            .contenedor-boton form {
+                                display: inline-block;
+                            }
+                    </style>
+                <div class='contenedor-boton'>        
+                    <form method='POST' action='../../../backend/src/noticias/eliminar_noticia.php' onsubmit='return confirm(\"¿Estás seguro de que quieres borrar esta noticia?\");'>
+                        <input type='hidden' name='id_noticias' value='$id_noticias'>
+                        <button type='submit' class='btn-borrar' title='Eliminar noticia'
+                            style='margin-top:20px; background-color: #ffffff; color:white; padding:10px 20px; border:none; cursor:pointer; display: flex; align-items: center;'>
+                        
+                            <img src='../../../web/etc/assets/img/basura_cerrada.png' class='icon-cerrado' alt='Papelera cerrada'>
+                                
+                            <img src='../../../web/etc/assets/img/basura_abierta.png' class='icon-abierto' alt='Papelera abierta'>
+                        
+                        </button>
+                    </form>
+                </div>    
             </div>";
     } else {
         echo "<p>Noticia no encontrada.</p>";
